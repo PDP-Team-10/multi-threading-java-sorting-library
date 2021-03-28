ad35f5f8e7b465b871d43fcfaf982eefc96c250a
Michael <michaelortiz05@hotmail.com>
2021-03-28 13:59:34 -0400

    Started work on parallel insertion sort

--- InsertionSort.java
+++ InsertionSort.java
@@ -1,5 +1,22 @@
-public class InsertionSort 
+public class InsertionSort extends Thread
 {
+
+    private int[] arr;
+    private int start, end;
+
+    public InsertionSort(int[] arr, int start, int end)
+    {
+        this.arr = arr;
+        this.start = start;
+        this.end = end;
+    }
+
+    @Override
+    public void run()
+    {   
+        insertionSortHelper(arr, start, end);
+    }
+
     public static void insertionSort(int arr[])
     {
         int n = arr.length;
@@ -15,5 +32,37 @@ public class InsertionSort
             }
             arr[j + 1] = k;
         }
+    }
+
+    public static void insertionSortHelper(int[] arr, int start, int end)
+    {
+        for (int i = start + 1; i < end; i++)
+        {
+            int k = arr[i];
+            int j = i - 1;
+
+            while (j >= start && arr[j] > k)
+            {
+                arr[j + 1] = arr[j];
+                j = j - 1;
+            }
+            arr[j + 1] = k;
+        }
+    }
+
+    public static void concurrentInsertionSort(int[] arr, int start, int end) throws InterruptedException
+    {
+        //arr = {0,0,0,0,0,0,0,0,0,0};
+        int processors = Runtime.getRuntime().availableProcessors();
+        int workload = (arr.length + processors - 1) / processors;
+        for (int i = 0; i < processors; i++) 
+        {
+            int low = i * workload;
+            int high = Math.min(start + workload, arr.length);
+            InsertionSort is = new InsertionSort(arr, low, high);
+            is.start();
+        }
+        // Next step: join remaining threads before merging
+        // Change implementatino to use threadpool
     }
 }
