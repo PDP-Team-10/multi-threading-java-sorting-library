2a3d11ec72e6af79c7969b9b7aa39326d8bd9bbf
Michael <michaelortiz05@hotmail.com>
2021-04-01 20:02:45 -0400

    Minor insertion sort changes

--- InsertionSort.java
+++ InsertionSort.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Random;
 public class InsertionSort extends Thread {
     private int[] arr;
     private int start, end;
@@ -9,8 +10,15 @@ public class InsertionSort extends Thread {
         this.end = end;
     }
 
+    public int[] getArr() { return arr;}
+
+    private static void printArr(int[] arr) {
+        for (Integer e : arr) System.out.print(e + " ");
+        System.out.println();
+    }
+
     @Override
-    public void run() {insertionSortHelper();}
+    public void run() { insertionSortHelper();}
 
     public static void concurrentInsertionSort(int[] arr) throws InterruptedException{
        // int processors = Math.min(Runtime.getRuntime().availableProcessors(), arr.length);
@@ -27,21 +35,17 @@ public class InsertionSort extends Thread {
 
             pool.add(i, new InsertionSort(arr, low, high));
             pool.get(i).start();
-            pool.get(i).join();
+           // pool.get(i).join();
         }
 
         // *Improve this by using a proper thread pool
         for (InsertionSort is : pool) is.join();
+        
 
         // Testing parallel insertion sort
         //for (InsertionSort is : pool) printArr(is.getArr()); 
-    }
 
-    public int[] getArr() {return arr;}
-
-    private static void printArr(int[] arr) {
-        for (Integer e : arr) System.out.print(e + " ");
-        System.out.println();
+        /* Consider using a minheap to merge the arrays */
     }
 
     public static void insertionSort(int arr[]) {
@@ -59,7 +63,7 @@ public class InsertionSort extends Thread {
     }
 
     private void insertionSortHelper() {
-        printArr(arr);
+        
         for (int i = start + 1; i < end; i++) {
             int k = arr[i];
             int j = i - 1;
@@ -70,11 +74,15 @@ public class InsertionSort extends Thread {
             }
             arr[j + 1] = k;
         }
-
     }
 
-/*  public static void main(String[] args) throws InterruptedException{
-        //int[] test = {2,1,4,3,6,5,8,7,10,9,12,11,14,13,16,15};
+    /*public static void main(String[] args) throws InterruptedException{
+        // int[] test = {2,1,4,3,6,5,8,7,10,9,12,11,14,13,16,15};
+        int[] test = new int[50];
+        Random rand = new Random();
+        for (int i = 0; i < test.length; i++) test[i] = rand.nextInt(50);
+        printArr(test);
+        System.out.println();
         InsertionSort.concurrentInsertionSort(test);
     }*/
 }
