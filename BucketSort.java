7202073d68fa0437fe3b2f3c3466ff985ca92c48
stefanwerleman <stefanwerleman@knights.ucf.edu>
2021-04-20 15:08:46 -0400

    Added a Max and Min concrurrent class

--- BucketSort.java
+++ BucketSort.java
@@ -2,39 +2,82 @@ import java.io.*;
 import java.util.*;
 import java.lang.*;
 import java.lang.Runtime;
-import java.rmi.RemoteException;
-import java.util.concurrent.Future;
 import java.util.concurrent.TimeUnit;
-import java.util.concurrent.Callable;
-import java.util.concurrent.Executors;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.locks.ReentrantLock;
+
+import javax.xml.validation.ValidatorHandler;
 
 // Value Object: Contains an integer and lock
-class Value
-{
-    public Integer value;
-    public ReentrantLock lock;
+// class Value
+// {
+//     public Integer value;
 
-    Value ()
+//     Value ()
+//     {
+//         this.value = 0;
+//     }
+
+//     public String toString()
+//     {
+//         return "(" + this.value + ")";
+//     }
+// }
+
+class Max extends Thread
+{
+    private int [] array; 
+    public int max;
+
+    Max (int [] array)
     {
-        this.value = 0;
-        this.lock = new ReentrantLock();
+        this.array = array;
+        this.max = Integer.MIN_VALUE;
     }
 
-    public String toString()
+    public void run()
     {
-        return "(" + this.value + ")";
+        for (int i = 0; i < this.array.length; i++)
+        {
+            if (this.array[i] > this.max)
+            {
+                this.max = this.array[i];
+            }
+        }
     }
 }
 
-
-public class BucketSort
+class Min extends Thread
 {
-    public int index = 0;
-    public Value [] bucket;
-    public Integer min, max;
+    private int [] array; 
+    public int min;
+
+    Min (int [] array)
+    {
+        this.array = array;
+        this.min = Integer.MAX_VALUE;
+    }
+
+    public void run()
+    {
+        for (int i = 0; i < this.array.length; i++)
+        {
+            if (this.array[i] < this.min)
+            {
+                this.min = this.array[i];                
+            }
+        }
+    }
+}
+
+public class BucketSort extends Thread
+{
+    private int min, max;
+    private int [] bucket;
+    private int [] array;
+
+    public void run()
+    {
+
+    }
 
     public void sleep(int seconds)
     {
@@ -48,138 +91,19 @@ public class BucketSort
         }
     }
 
-    public void maximum(int [] array)
+    public void sort(int [] array) throws InterruptedException
     {
-        max = Integer.MIN_VALUE;
-        for (int i = 0; i < array.length; i++)
-        {
-            if (array[i] > max)
-            {
-                max = array[i];
-            }
-        }
-    }
+        Max tmax = new Max(array);
+        Min tmin = new Min(array);
+        tmax.start();
+        tmin.start();
+        tmax.join();
+        tmin.join();
 
-    public void minimum(int [] array)
-    {
-        min = Integer.MAX_VALUE;
-        for (int i = 0; i < array.length; i++)
-        {
-            if (array[i] < min)
-            {
-                min = array[i];                
-            }
-        }
-    }
-
-    public void sortSequential(int [] array)
-    {
-        // Both O(n)
-        maximum(array);
-        minimum(array);
-
-        int bucketSize = max - min + 1;
         
-        // O(m)
-        bucket = new Value[bucketSize];
-
-        for (int i = 0; i < bucketSize; i++)
-        {
-            bucket[i] = new Value();
-        }
-
-        // O(n)
-        for (int i = 0; i < array.length; i++)
-        {
-            int k = array[i] - min;
-            bucket[k].value++;
-        }
-
-        int j = 0;
-        
-        // O(m)
-        for (int i = 0; i < bucket.length; i++)
-        {
-            while (bucket[i].value > 0)
-            {
-                bucket[i].value--;
-                array[j] = i + min;
-                j++;
-            }
-        }
     }
 
-    public void sort(int [] array)
-    {
-        maximum(array);
-        minimum(array);
-
-        int bucketSize = max - min + 1;
-
-        bucket = new Value[bucketSize];
-
-        for (int i = 0; i < bucketSize; i++)
-        {
-            bucket[i] = new Value();
-        }
-
-        Callable<Value []> cache = () ->
-        {
-            int key = array[index] - min;
-
-            bucket[key].lock.lock();
-
-            try
-            {
-                bucket[key].value++;
-            }
-            finally
-            {
-                bucket[key].lock.unlock();
-                index++;
-                return bucket;
-            }
-        };
-
-        int numThreads = Runtime.getRuntime().availableProcessors();
-        ExecutorService executor = Executors.newFixedThreadPool(numThreads);
-
-        ArrayList<Future<Value []>> futures = new ArrayList<>();
-
-
-        for (int i = 0; i < array.length; i++)
-        {
-            futures.add(executor.submit(cache));
-        }
-
-        executor.shutdown();
-
-        for (Future<Value []> f: futures)
-        {
-            try 
-            {
-                bucket = f.get();
-            }
-            catch (InterruptedException | ExecutionException e)
-            {
-                e.printStackTrace();
-            }
-        }
-
-        int j = 0;
-
-        for (int i = 0; i < bucket.length; i++)
-        {
-            while (bucket[i].value > 0)
-            {
-                bucket[i].value--;
-                array[j] = i + min;
-                j++;
-            }
-        }
-    }
-
-    public static void main(String [] args) 
+    public static void main(String [] args) throws InterruptedException
     {
         int [] array = new int[(int)3];
 
@@ -188,19 +112,13 @@ public class BucketSort
             array[i] = (int)(Math.random() * 10) + 1;
         }
 
-        // System.out.println(Arrays.toString(array));
+        System.out.println(Arrays.toString(array));
 
         long start = System.currentTimeMillis();
-        
         BucketSort b = new BucketSort();
         b.sort(array);
-        // b.sortSequential(array);
-        
         long finish = System.currentTimeMillis();
         
         System.out.println("Finished: " + ((finish - start)) + " ms");
-        
-
-        // System.out.println(Arrays.toString(array));
     }
 }
