e83d7fad82ea843f2c420230c78ba28c42eec7b9
Michael <michaelortiz05@hotmail.com>
2021-04-23 11:30:05 -0400

    Removed isSorted() from ShellSort.java

--- ShellSort.java
+++ ShellSort.java
@@ -87,14 +87,6 @@ public class ShellSort <T extends Comparable<? super T>> extends Thread {
             }
         }
     }
-
-    public static <T extends Comparable<? super T>> boolean isSorted(List<T> list)
-    {       
-        for (int i = 1; i < list.size(); i++) {
-            if (list.get(i-1).compareTo(list.get(i)) > 0) return false;
-        }
-        return true;
-    }
 }
 
 // Source: Interviewdojo.com
