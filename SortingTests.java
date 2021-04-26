6e096e85de69ee2139dd9b1075e110503c68b031
Michael <michaelortiz05@hotmail.com>
2021-04-25 20:11:16 -0400

    modified sortingtests to test one algo at a time chosen by user and output results to files

--- SortingTests.java
+++ SortingTests.java
@@ -1,44 +1,112 @@
-import java.util.Arrays;
-import java.util.Random;
+import java.util.*;
+import java.io.FileWriter;  
+import java.io.IOException;
 
 public class SortingTests {
     
-    public static void main ( String[] args ) {
+    static void testShellSort(int arraySize) throws IOException, InterruptedException{
+        ArrayList<Integer> ascendingArray = new ArrayList<Integer>();
+        ArrayList<Integer> descendingArray = new ArrayList<Integer>();
+        ArrayList<Integer> shuffledArray = new ArrayList<Integer>();
 
-        // Create data to be sorted
-        int arraySize = (int)300;    
-        if (args.length > 0) {
-            try {
-                arraySize = Integer.parseInt(args[0]);
-            } catch (NumberFormatException e) {
-                System.err.println("Could not parse int from \"" + args[0] + "\"");
-                return;
-            }
-        }
-        int[] ascendingArray = new int[arraySize];
-        int[] shuffledArray = new int[arraySize];
-        int[] descendingArray = new int[arraySize];
-        
-        for (int i = 0; i < arraySize; i++) {
-            ascendingArray[i] = i;
-        }
-        
-        Random rand = new Random();
-        for (int i = 0; i < arraySize; i ++) {
-            shuffledArray[i] = rand.nextInt(arraySize);
-        }
-        
-        for (int i = 0; i < arraySize; i++) {
-            descendingArray[i] = arraySize - i - 1;
-        }
-        
+        FileWriter sequentialWriter = new FileWriter("sequential-shell-sort-results.txt");
+        FileWriter concurrentWriter = new FileWriter("concurrent-shell-sort-results.txt");
+        sequentialWriter.write("size\tascending(MS)\tdescending(MS)\tshuffled(MS)\n");
+        concurrentWriter.write("size\tascending(MS)\tdescending(MS)\tshuffled(MS)\n");
+
         // Record start and end times
         long startTime;
         long endTime;
+        long ascendingTime;
+        long descendingTime;
+        long shuffledTime;
         
-        int[] arrayCopy;
+        Random rand = new Random();
+
+        for (int iter = 0; iter < 10; iter++) {
+            for (int i = 0; i < arraySize * (iter + 1); i++) 
+                ascendingArray.add(i);
+        
+            for (int i = 0; i < arraySize * (iter + 1); i ++) 
+                shuffledArray.add(rand.nextInt(arraySize * (iter + 1)));
+            
+            
+            for (int i = 0; i < arraySize * (iter + 1); i++) 
+                descendingArray.add(arraySize * (iter + 1) - i - 1);
+            
+        
+            System.out.println("Testing sequential shell sort...");
+
+            // Test sequential
+            ArrayList arrayCopy = (ArrayList) ascendingArray.clone();
+            startTime = System.currentTimeMillis();
+            Sorter.shellSort(arrayCopy);
+            endTime = System.currentTimeMillis();
+            ascendingTime = endTime - startTime;
+
+            arrayCopy = (ArrayList) descendingArray.clone();
+            startTime = System.currentTimeMillis();
+            Sorter.shellSort(arrayCopy);
+            endTime = System.currentTimeMillis();
+            descendingTime = endTime - startTime;
+
+            arrayCopy = (ArrayList) shuffledArray.clone();
+            startTime = System.currentTimeMillis();
+            Sorter.shellSort(arrayCopy);
+            endTime = System.currentTimeMillis();
+            shuffledTime = endTime - startTime;
+
+            sequentialWriter.write(arraySize + "\t" + ascendingTime + "\t" + descendingTime + "\t" + shuffledTime + "\n");
+            System.out.println("testing concurrent shell sort...");
+
+            // Test concurrent
+            arrayCopy = (ArrayList) ascendingArray.clone();
+            startTime = System.currentTimeMillis();
+            Sorter.concurrentShellSort(arrayCopy);
+            endTime = System.currentTimeMillis();
+            ascendingTime = endTime - startTime;
+
+            arrayCopy = (ArrayList) descendingArray.clone();
+            startTime = System.currentTimeMillis();
+            Sorter.concurrentShellSort(arrayCopy);
+            endTime = System.currentTimeMillis();
+            descendingTime = endTime - startTime;
+
+            arrayCopy = (ArrayList) shuffledArray.clone();
+            startTime = System.currentTimeMillis();
+            Sorter.concurrentShellSort(arrayCopy);
+            endTime = System.currentTimeMillis();
+            shuffledTime = endTime - startTime;
+
+            concurrentWriter.write(arraySize + "\t" + ascendingTime + "\t" + descendingTime + "\t" + shuffledTime + "\n");
+
+            ascendingArray.clear();
+            descendingArray.clear();
+            shuffledArray.clear();
+            
+
+        }    
+        sequentialWriter.close();
+        concurrentWriter.close();
+        System.out.println("Results written to files.");
+    }
+    public static void main ( String[] args ) throws IOException, InterruptedException{
+
+        // Create data to be sorted
+        int arraySize = (int)300;  
+        
+        Scanner in = new Scanner(System.in);
+
+        do {
+            System.out.println("Enter list size (default 300)");
+            arraySize = in.nextInt();
+        } while (arraySize < 0);
+
+        
         
         System.out.println("Comparison of execution time for sequential and multi-threaded sorting algorithms");
+        testShellSort(arraySize);
+        in.close();
 /*        
         // Merge sort
         System.out.println("[ Merge sort ]");
@@ -101,7 +169,7 @@ public class SortingTests {
         endTime = System.currentTimeMillis();
         System.out.println("\tSorted descending array in\t" + (endTime - startTime) + " ms");
         checkSortedList(descendingArray, arrayCopy);
-*/
+
         // Quick sort
         System.out.println("[ Quick sort ]");
         // Sequential
@@ -340,7 +408,7 @@ public class SortingTests {
         // System.out.println("\tSorted descending array in\t" + (endTime - startTime) + " ms");
         // checkSortedList(descendingArray, arrayCopy);
 
-        
+        */
     }
 
     public static void checkSortedList(int[] original, int[] candidate) {
