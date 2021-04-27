4e37069f371c0aa17db5c693b9da6cc6c74c139f
Michael <michaelortiz05@hotmail.com>
2021-04-26 23:19:47 -0400

    final countdown

--- SortingTests.java
+++ SortingTests.java
@@ -6,16 +6,42 @@ public class SortingTests {
 
         // Create data to be sorted
         int arraySize = (int)500000;  
-        
+        int algoChoice = 0;
         Scanner in = new Scanner(System.in);
 
         do {
             System.out.println("Enter list size (default 50,000)");
             arraySize = in.nextInt();
         } while (arraySize < 0);
-   
+        
+
         System.out.println("Comparison of execution time for sequential and multi-threaded sorting algorithms\n");
-        //ShellSort.testShellSort(arraySize);
+
+        do {
+            System.out.println("Select a sorting algorithm to test:\n1. Merge Sort\n2. Quick Sort\n3. Bucket Sort\n4. Shell Sort\n5. Selection Sort");
+            algoChoice = in.nextInt();
+
+            switch (algoChoice) {
+                case 1:
+                    MergeSort.testMergeSort(arraySize);
+                    break;
+                case 2:
+                    QuickSort.testQuickSort(arraySize);
+                    break;
+                case 3:
+                    BucketSort.testBucketSort(arraySize);
+                    break;
+                case 4:
+                    ShellSort.testShellSort(arraySize);
+                    break;
+                case 5:
+                    SelectionSort.testSelectionlSort(arraySize);
+                    break;
+                default:
+                    System.out.println("Wrong input");
+            }
+
+        } while (algoChoice <= 0 || algoChoice > 5);
         in.close();
     }
 }
